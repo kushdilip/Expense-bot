33e84b2278f9266ab365056b8d12e75f34f975e0
dilip kushwaha <kush.dilip@gmail.com>
2013-12-10 15:31:02 +0530

    datepicket completed

--- ExpenseBot/src/com/hacknight/expensebot/activity/EditEntryActivity.java
+++ ExpenseBot/src/com/hacknight/expensebot/activity/EditEntryActivity.java
@@ -3,10 +3,12 @@ package com.hacknight.expensebot.activity;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -26,12 +28,13 @@ import com.hacknight.expensebot.model.Transaction;
 public class EditEntryActivity extends Activity {
 
 	EditText edit_amount;
-	DatePicker edit_date;
 	EditText edit_details;
 	RadioGroup edit_type;
 	Button ok_button;
 	Button cancel_button;
-	
+	Button datepicker;
+	final Calendar c = Calendar.getInstance();
+
 	float amount;
 	String date;
 	String details;
@@ -46,16 +49,42 @@ public class EditEntryActivity extends Activity {
 		
 		context = this;
 		final DBHandler handler = new DBHandler(this);
-		
+
 		edit_amount = (EditText) findViewById(R.id.edit_amount);
-		edit_date = (DatePicker) findViewById(R.id.edit_date);
 		edit_details = (EditText) findViewById(R.id.edit_details);
 		edit_type = (RadioGroup) findViewById(R.id.edit_type);
 		ok_button = (Button) findViewById(R.id.ok_button);
 		cancel_button = (Button) findViewById(R.id.cancel_button);
+		datepicker = (Button) findViewById(R.id.datepicker);
 
-		Calendar c = Calendar.getInstance();
-		edit_date.setCalendarViewShown(false);
+		
+
+		final DatePickerDialog.OnDateSetListener datePicker = new DatePickerDialog.OnDateSetListener() {
+
+		    @Override
+		    public void onDateSet(DatePicker view, int year, int monthOfYear,
+		            int dayOfMonth) {
+		        // TODO Auto-generated method stub
+		        c.set(Calendar.YEAR, year);
+		        c.set(Calendar.MONTH, monthOfYear);
+		        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+		        updateLabel();
+		    }
+
+		};
+
+		datepicker.setOnClickListener(new OnClickListener() {
+
+		        @Override
+		        public void onClick(View v) {
+		            // TODO Auto-generated method stub
+		            new DatePickerDialog(EditEntryActivity.this, datePicker, c
+		                    .get(Calendar.YEAR), c.get(Calendar.MONTH),
+		                    c.get(Calendar.DAY_OF_MONTH)).show();
+		        }
+		    });
+
+		
 		ActionBar actionBar = this.getActionBar();
 
 		Bundle extras = getIntent().getExtras();
@@ -73,19 +102,18 @@ public class EditEntryActivity extends Activity {
 				Calendar cal = Calendar.getInstance();
 				cal.setTime(date);
 				
-				edit_date.updateDate(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DATE));
 				
+				datepicker.setText(cal.get(Calendar.YEAR)+"/"+
+						cal.get(Calendar.MONTH)+"/"+cal.get(Calendar.DATE));
+
 			} catch (java.text.ParseException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
-		}
-		else {
+
+		} else {
 			actionBar.setTitle("Add New Transaction");
 		}
-		
-		
 
 		ok_button.setOnClickListener(new OnClickListener() {
 
@@ -94,8 +122,10 @@ public class EditEntryActivity extends Activity {
 				// TODO Auto-generated method stub
 				String str_amount = edit_amount.getText().toString();
 				amount = Float.valueOf(str_amount);
-				date = edit_date.getDayOfMonth() + "/" + edit_date.getMonth()
-						+ "/" + edit_date.getYear();
+//				date = edit_date.getDayOfMonth() + "/" + edit_date.getMonth()
+//						+ "/" + edit_date.getYear();
+				
+				
 				details = edit_details.getText().toString();
 				switch (edit_type.getCheckedRadioButtonId()) {
 				case R.id.cash:
@@ -105,19 +135,19 @@ public class EditEntryActivity extends Activity {
 					accountKind = 2;
 					break;
 				}
-				
-				
-//				handler.addTransaction(new Transaction(amount, date, " ",type, false));
-				handler.addTransaction(new Transaction(amount, date, accountKind));
-				
-				
+
+				// handler.addTransaction(new Transaction(amount, date,
+				// " ",type, false));
+				handler.addTransaction(new Transaction(amount, date,
+						accountKind));
+
 				int count = handler.getTransactionCount();
-				
-				Toast.makeText(context, " " + count ,Toast.LENGTH_LONG).show();
-				
+
+				Toast.makeText(context, " " + count, Toast.LENGTH_LONG).show();
+
 				Intent intent = new Intent(context, MainActivity.class);
 				context.startActivity(intent);
-				
+
 			}
 
 		});
@@ -129,5 +159,13 @@ public class EditEntryActivity extends Activity {
 				context.startActivity(intent);
 			}
 		});
+	}
+
+	private void updateLabel() {
+
+		String myFormat = "dd/MM/yy"; // In which you need put here
+		SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+		date = sdf.format(c.getTime());
+		datepicker.setText(date);
 	}
 }
