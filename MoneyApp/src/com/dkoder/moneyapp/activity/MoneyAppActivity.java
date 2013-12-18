45dfc6c5e0d8e56a2ffacf8b3ac74bd3bc6a6e53
Dilip Kushwaha <kush.dilip@gmail.com>
2013-12-19 02:26:16 +0530

    sliding menu complete without fragments

--- MoneyApp/src/com/dkoder/moneyapp/activity/MoneyAppActivity.java
+++ MoneyApp/src/com/dkoder/moneyapp/activity/MoneyAppActivity.java
@@ -2,12 +2,17 @@ package com.dkoder.moneyapp.activity;
 
 import java.util.ArrayList;
 
+import android.app.Fragment;
+import android.content.res.Configuration;
 import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.dkoder.moneyapp.R;
@@ -41,8 +46,12 @@ public class MoneyAppActivity extends FragmentActivity {
 
 		// load slide menu items
 		navMenuTitles = getResources().getStringArray(R.array.nav_drawer_items);
+
 		navMenuIcons = getResources()
 				.obtainTypedArray(R.array.nav_drawer_icons);
+
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerList = (ListView) findViewById(R.id.list_slidermenu);
 
 		navDrawerItems = new ArrayList<NavDrawerItem>();
 
@@ -60,6 +69,39 @@ public class MoneyAppActivity extends FragmentActivity {
 		navDrawerItems.add(new NavDrawerItem(navMenuTitles[3], navMenuIcons
 				.getResourceId(3, -1)));
 
+		// Recycle the typed array.
+		navMenuIcons.recycle();
+
+		// setting the nav drawer list adapter
+		adapter = new NavDrawerListAdapter(getApplicationContext(),
+				navDrawerItems);
+
+		mDrawerList.setAdapter(adapter);
+
+		mDrawerList.setOnItemClickListener(new SlideMenuClickListener());
+
+		// enabling action bar app icon and behaving it as toggle button
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+		
+		mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+				R.drawable.ic_drawer, R.string.app_name, R.string.app_name) {
+			public void onDrawerClosed(View view) {
+				getActionBar().setTitle(mTitle);
+				// /calling onPrepareOptionMenu() to show action bar icons
+				invalidateOptionsMenu();
+			}
+
+			public void onDrawerOpened(View drawerView) {
+				getActionBar().setTitle(mDrawerTitle);
+				invalidateOptionsMenu();
+			}
+		};
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+		
+		if (savedInstanceState == null) {
+			displayView(0);
+		}
 	}
 
 	@Override
@@ -68,5 +110,74 @@ public class MoneyAppActivity extends FragmentActivity {
 		getMenuInflater().inflate(R.menu.money_app, menu);
 		return true;
 	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
 
+		switch (item.getItemId()) {
+		case R.id.action_settings:
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		// TODO Auto-generated method stub
+		super.onConfigurationChanged(newConfig);
+		mDrawerToggle.onConfigurationChanged(newConfig);
+	}
+
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onPostCreate(savedInstanceState);
+		mDrawerToggle.syncState();
+	}
+
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+		menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
+		return super.onPrepareOptionsMenu(menu);
+	}
+
+	@Override
+	public void setTitle(CharSequence title) {
+		// TODO Auto-generated method stub
+		mTitle = title;
+		getActionBar().setTitle(mTitle);
+	}
+
+	/**
+	 * Diplaying fragment view for selected nav drawer list item
+	 * */
+	private void displayView(int position) {
+		// update the main content by replacing fragments
+		Fragment fragment = null;
+		switch (position) {
+
+		default:
+				break;
+		}
+		
+	}
+
+	
+
+	private class SlideMenuClickListener implements
+			ListView.OnItemClickListener {
+
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {
+			displayView(position);
+		}
+
+	}
 }
