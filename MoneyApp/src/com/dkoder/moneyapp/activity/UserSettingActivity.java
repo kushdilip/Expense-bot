46f28d79a60a3ee2d6b8ed5207d9284cb895f483
dilip <dkoder.dk@gmail.com>
2014-01-24 10:00:37 +0530

    about added in settings

--- MoneyApp/src/com/dkoder/moneyapp/activity/UserSettingActivity.java
+++ MoneyApp/src/com/dkoder/moneyapp/activity/UserSettingActivity.java
@@ -1,6 +1,9 @@
 package com.dkoder.moneyapp.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 
 import com.dkoder.moneyapp.R;
@@ -12,7 +15,18 @@ public class UserSettingActivity extends PreferenceActivity {
         super.onCreate(savedInstanceState);
  
         addPreferencesFromResource(R.xml.settings);
- 
+        
+        Preference preference = findPreference("prefRateUs");
+        preference.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+			
+			@Override
+			public boolean onPreferenceClick(Preference arg0) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=com.dkoder.moneyapp"));
+				startActivity(intent);
+				return false;
+			}
+		});
     }
 	
 }
